7c56c92383bc4e412d2774101d32f787e33b4c96
Alana Nardes <alana.nardes.dev@gmail.com>
2025-05-23 18:02:39 +0000

    Adicionado novos Atributos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,6 +9,8 @@ int main() {
     float area1, area2;
     float pib1, pib2;
     int pontosTuristicos1, pontosTuristicos2;
+    float densidadePopulacional1, densidadePopulacional2;
+    float pibperCapita1, pibperCapita2;
 
     printf("Pronto para cadastrar as Cartas do Jogo? \n\n"); //2x "\n", para pular 2 linhas. 
 
@@ -25,14 +27,17 @@ int main() {
     printf("(CARTA 1) Digite qual a População da Cidade: \n"); //Digitar sem pontos, apenas numeros.
     scanf("%d", &populacao1);
 
-    printf("(CARTA 1) Digite a Area em km² da Cidade: \n"); //Digitar numeros, porém com 3 casas decimais.
+    printf("(CARTA 1) Digite a Area em km² da Cidade: \n"); //Digitar numeros, porém com 2 casas decimais.
     scanf("%f", &area1);
 
-    printf("(CARTA 1) Digite o PIB da Cidade: \n"); //Digitar numeros, porém com 3 casas decimais.
+    printf("(CARTA 1) Digite o PIB da Cidade: \n"); //Digitar numeros, porém com 2 casas decimais.
     scanf("%f", &pib1);
 
     printf("(CARTA 1) Digite a quantidade de Pontos Turisticos da Cidade: \n"); //Apenas numeros.
     scanf("%d", &pontosTuristicos1);
+
+    densidadePopulacional1 = (float) populacao1 / area1; //Cálculo da Densidade Populacional (Numero de Pessoas / por km²), com conversão Explicita (Casting).
+    pibperCapita1 = (float) pib1 / populacao1; //Cálculo do PIB per Capita (Valor do PIB / Nº de Pessoas), com conversão Explicita (Casting).
 
     printf("\n");
 
@@ -42,9 +47,11 @@ int main() {
     printf("Código da Carta: %s\n", codigo1);
     printf("Nome da Cidade: %s\n", cidade1);
     printf("População: %d Pessoas\n", populacao1);
-    printf("Área em km²: %.3fkm²\n", area1); //%.3f = Indificação de 3 casas decimais.
-    printf("PIB: R$%.3f\n", pib1); //%.3f = Indificação de 3 casas decimais.
+    printf("Área em km²: %.2fkm²\n", area1); //%.2f = Indificação de 2 casas decimais.
+    printf("PIB: R$%.2f\n", pib1); //%.2f = Indificação de 2 casas decimais.
     printf("Pontos Turisticos na Cidade: %d\n", pontosTuristicos1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional1);
+    printf("PIB per Capita: R$%.2F\n", pibperCapita1);
     
 
     printf("\n\n");
@@ -62,14 +69,17 @@ int main() {
     printf("(CARTA 2) Digite qual a População da Cidade: \n"); //Digitar sem pontos, apenas numeros.
     scanf("%d", &populacao2);
 
-    printf("(CARTA 2) Digite a Area em km² da Cidade: \n"); //Digitar numeros, porém com 3 casas decimais.
+    printf("(CARTA 2) Digite a Area em km² da Cidade: \n"); //Digitar numeros, porém com 2 casas decimais.
     scanf("%f", &area2);
 
-    printf("(CARTA 2) Digite o PIB da Cidade: \n"); //Digitar numeros, porém com 3 casas decimais.
+    printf("(CARTA 2) Digite o PIB da Cidade: \n"); //Digitar numeros, porém com 2 casas decimais.
     scanf("%f", &pib2);
 
     printf("(CARTA 2) Digite a quantidade de Pontos Turisticos da Cidade: \n"); //Apenas numeros.
     scanf("%d", &pontosTuristicos2);
+
+    densidadePopulacional2 = (float) populacao2 / area2; //Cálculo da Densidade Populacional (Nº de Pessoas / por km²), com conversão Explicita (Casting).
+    pibperCapita2 = (float) pib2 / populacao2; //Cálculo do PIB per Capita (Valor do PIB / Nº de Pessoas), com conversão Explicita (Casting).
 
     printf("\n");
     
@@ -79,9 +89,11 @@ int main() {
     printf("Código da Carta: %s\n", codigo2);
     printf("Nome da Cidade: %s\n", cidade2);
     printf("População: %d Pessoas\n", populacao2);
-    printf("Área em km²: %.3fkm²\n", area2); //%.3f = Indificação de 3 casas decimais.
-    printf("PIB: R$%.3f\n", pib2); //%.3f = Indificação de 3 casas decimais.
+    printf("Área em km²: %.2fkm²\n", area2); //%.2f = Indificação de 2 casas decimais.
+    printf("PIB: R$%.2f\n", pib2); //%.2f = Indificação de 2 casas decimais.
     printf("Pontos Turisticos na Cidade: %d\n", pontosTuristicos2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional2);
+    printf("PIB per Capita: R$%.2F\n", pibperCapita2);
 
     printf("\n\n"); //2x "\n" Para Encerramento do Programa.
 
